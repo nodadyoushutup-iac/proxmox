ec2a6e65c56028a7a2838ce8d02b9b53b9e4e1cb
nodadyoushutup <admin@nodadyoushutup.com>
2024-08-19 04:26:20 +0000

    [WIP] Proxmox

--- provider.tf
+++ provider.tf
@@ -5,27 +5,20 @@ terraform {
     proxmox = {
       source = "bpg/proxmox"
     }
-    spacelift = {
-      source  = "spacelift-io/spacelift"
-      version = ">= 0.2.0"
-    }
   }
 }
-
 provider "proxmox" {
   endpoint = local.provider.proxmox.endpoint
   api_token = local.provider.proxmox.api_token
   insecure  = local.provider.proxmox.insecure
   ssh {
-    agent = true
-    agent_socket = 22
-    username = local.provider.proxmox.username
+    agent = local.provider.proxmox.ssh.agent
+    agent_socket = local.provider.proxmox.ssh.agent_socket
+    username = local.provider.proxmox.ssh.username
     private_key = file("/mnt/workspace/proxmox.pem")
     node {
-      name = local.provider.proxmox.node.name
-      address = local.provider.proxmox.node.address
+      name = local.provider.proxmox.ssh.node.name
+      address = local.provider.proxmox.ssh.node.address
     }
   }
 }
-
-provider "spacelift" {}
